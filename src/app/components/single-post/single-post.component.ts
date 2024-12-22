5677a08a826bc8a305b6948be14a073bef9b7316
being-rohan <you@example.com>
2024-12-22 16:49:14 +0530

    k

--- src/app/components/single-post/single-post.component.ts
+++ src/app/components/single-post/single-post.component.ts
@@ -46,6 +46,8 @@ export class SinglePostComponent implements OnInit {
         this._Ser.ondelete(this.postid).subscribe((res) => {
           console.log(res);
           this._router1.navigate(['/home']);
+          this._matsanck.matsancopen(` See the Full deatils of ${this.postObj.name} `)
+
         });
       }
     });
